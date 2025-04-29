8f6952474e07a7fff7aada3e7888a3e2b13fa3ec
Alaka1996 <bgalakananda@gmail.com>
2025-04-29 10:46:05 +0530

    Update cpp_trial.c

--- cpp_trial.c
+++ cpp_trial.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
-int main() // MISRA: function should have explicit return type and parameters should be specified
-{
-    int i; // MISRA: variable not initialized
+int main() 
+    int i; 
     int arr[5];
     int x = 0;
     int y = 10;
     int result;
 
-    // Buffer overflow: writing beyond the bounds of arr
-    for (i = 0; i <= 5; i++) // MISRA: should use <, not <=
+    
+    for (i = 0; i <= 5; i++) 
     {
-        arr[i] = i * 2; // When i == 5, arr[5] is out of bounds
+        arr[i] = i * 2; 
     }
 
-    // Division by zero
+   
     if (x == 0)
     {
-        result = y / x; // Division by zero
+        result = y / x; 
         printf("Result: %d\n", result);
     }
 
-    // MISRA violation: use of goto
+    
     goto end;
 
     printf("This line will be skipped\n");
