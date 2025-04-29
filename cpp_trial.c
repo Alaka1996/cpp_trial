1dd1a023c546aa7cc290d8055f9c3e6897eab403
Alaka1996 <bgalakananda@gmail.com>
2025-04-29 10:33:09 +0530

    Update cpp_trial.c

--- cpp_trial.c
+++ cpp_trial.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
-#define MAX_CELLS 4
-/* Function to compute average cell voltage */
-float compute_average(int voltages[], int num_cells)
+
+int main() // MISRA: function should have explicit return type and parameters should be specified
 {
-    int sum = 0;
-    int i;
-    for (i = 0; i <= num_cells; i++)  // Off-by-one error (buffer overflow)
+    int i; // MISRA: variable not initialized
+    int arr[5];
+    int x = 0;
+    int y = 10;
+    int result;
+
+    // Buffer overflow: writing beyond the bounds of arr
+    for (i = 0; i <= 5; i++) // MISRA: should use <, not <=
     {
-        sum += voltages[i];
+        arr[i] = i * 2; // When i == 5, arr[5] is out of bounds
     }
-    if (num_cells == 0)
+
+    // Division by zero
+    if (x == 0)
     {
-        return 0;  // Potential division by zero below
+        result = y / x; // Division by zero
+        printf("Result: %d\n", result);
     }
-    return (float)sum / num_cells;
-}
-int main(void)
-{
-    int cell_voltages[MAX_CELLS] = {3700, 3650, 3720, 3680};
-    int num_cells = 4;
-    float avg_voltage;
-    avg_voltage = compute_average(cell_voltages, num_cells);
-    printf("Average cell voltage: %.2f mV\n", avg_voltage);
+
     // MISRA violation: use of goto
-    if (avg_voltage < 3600)
-    {
-        goto LOW_VOLTAGE;
-    }
-    printf("Battery health: OK\n");
+    goto end;
+
+    printf("This line will be skipped\n");
+
+end:
     return 0;
-LOW_VOLTAGE:
-    printf("Warning: Low cell voltage detected!\n");
-    return 1;
+}
