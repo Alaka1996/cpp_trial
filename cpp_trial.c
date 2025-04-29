e7f79ef26169b09def0c657462cbac7bd3efcd6b
Alaka1996 <bgalakananda@gmail.com>
2025-04-29 11:12:22 +0530

    Update cpp_trial.c

--- cpp_trial.c
+++ cpp_trial.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 
-int main() 
-    int i; 
-    int arr[5];
-    int x = 0;
-    int y = 10;
-    int result;
-
-    
-    for (i = 0; i <= 5; i++) 
-    {
-        arr[i] = i * 2; 
+int get_array_value(int *arr, int len, int idx) {
+    if (idx >= 0 && idx < len) {
+        return arr[idx];
     }
+    return -1;
+}
+
+int main() {
+    int my_array[3] = {10, 20, 30};
+    int *ptr = NULL;
+    int index = 3;
 
    
-    if (x == 0)
-    {
-        result = y / x; 
-        printf("Result: %d\n", result);
-    }
+    int value1 = get_array_value(my_array, 3, index);
+    printf("Value1: %d\n", value1);
 
     
-    goto end;
+    int value2 = get_array_value(ptr, 3, 1);
+    printf("Value2: %d\n", value2);
 
-    printf("This line will be skipped\n");
-
-end:
     return 0;
 }
